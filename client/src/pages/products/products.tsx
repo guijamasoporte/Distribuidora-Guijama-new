b29d603bf886a14b4b9ee17438d005c9547f3db4
Jona Garcia <jalexisagarcia.91@gmail.com>
2024-03-04 17:34:44 -0300

    migrationTypescript

---
+++ client/src/pages/products/products.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+interface GuijamaPageProps {
+
+}
+
+const GuijamaPage: React.FC<GuijamaPageProps> = () => {
+  return (
+    <div>
+      <h1>Bienvenido a Guijama</h1>
+      <p>Esta es la página de Guijama</p>
+    </div>
+  );
+}
+
+export default GuijamaPage;
