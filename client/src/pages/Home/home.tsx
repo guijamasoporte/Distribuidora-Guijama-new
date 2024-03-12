05b38680730af12f9a9eb6b40575058ed1e496b0
Jona Garcia <jalexisagarcia.91@gmail.com>
2024-03-12 14:45:37 -0300

    styles

--- client/src/pages/Home/home.tsx
+++ client/src/pages/Home/home.tsx
@@ -1,9 +1,49 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import InstanceOfAxios from "../../utils/intanceAxios";
+import { Product } from "../../interfaces/interfaces";
+import styles from "./home.module.css";
 
 const Home: React.FC = () => {
+  const [products, setProducts] = useState<Product[]>([]);
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      try {
+        const response: any = await InstanceOfAxios("/products", "GET");
+        setProducts(response);
+      } catch (error) {
+        console.error("Error fetching products:", error);
+      }
+    };
+
+    fetchProducts();
+  }, []);
+
   return (
-    <div>
-      <p>home</p>
+    <div className={styles.productCatalog}>
+      <h2>Productos Destacados</h2>
+      <div className={styles.productList}>
+        {products.map((product) => (
+          <div key={product.code} className={styles.product}>
+            <p>Titulo: {product.title}</p>
+            <p>Stock: {product.stock}</p>
+            <p>Precio: {product.priceList}</p>
+            <p>Categoría: {product.category}</p>
+            <p>Marca: {product.brand}</p>
+            <div className={styles.imageContainer}>
+              {Array.isArray(product.image) &&
+                product.image.map((imageUrl, index) => (
+                  <img
+                    key={index}
+                    src={imageUrl}
+                    alt={`${product.title}-${index}`}
+                    className={styles.productImage}
+                  />
+                ))}
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
