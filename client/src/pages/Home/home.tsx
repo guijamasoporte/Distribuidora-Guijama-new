3e6e5f489f391024b1d598bd16961ee42fefadd6
Jona Garcia <jalexisagarcia.91@gmail.com>
2024-03-13 16:52:57 -0300

    catalogue

--- client/src/pages/Home/home.tsx
+++ client/src/pages/Home/home.tsx
@@ -1,10 +1,12 @@
 import React, { useEffect, useState } from "react";
 import InstanceOfAxios from "../../utils/intanceAxios";
 import { Product } from "../../interfaces/interfaces";
+import KeyboardDoubleArrowUpIcon from "@mui/icons-material/KeyboardDoubleArrowUp";
 import styles from "./home.module.css";
 
 const Home: React.FC = () => {
   const [products, setProducts] = useState<Product[]>([]);
+  const [showScrollButton, setShowScrollButton] = useState(false);
 
   useEffect(() => {
     const fetchProducts = async () => {
@@ -17,6 +19,20 @@ const Home: React.FC = () => {
     };
 
     fetchProducts();
+
+    const handleScroll = () => {
+      if (window.scrollY > 100) {
+        setShowScrollButton(true);
+      } else {
+        setShowScrollButton(false);
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
   }, []);
 
   const productsByCategory: { [category: string]: Product[] } = {};
@@ -27,54 +43,71 @@ const Home: React.FC = () => {
     productsByCategory[product.category].push(product);
   });
 
+  const sortedCategories = Object.keys(productsByCategory).sort();
+
+  const scrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+  };
+
   return (
     <div className={styles.productCatalog}>
+      {showScrollButton && (
+        <button className={styles.scrollToTopButton} onClick={scrollToTop}>
+          <KeyboardDoubleArrowUpIcon />
+        </button>
+      )}
       <p className={styles.titleCatalogue}>Catálogo de Productos</p>
       <p className={styles.descriptionCatalogue}>
-        Bienvenido al catálogo de productos de nuestra tienda de tabaquería.
-        Aquí encontrarás una amplia selección de los mejores productos
-        relacionados con tabaco, puros, accesorios y más. ¡Explora nuestras
-        ofertas y descubre todo lo que necesitas para disfrutar de una
-        experiencia única!
+        Bienvenido a <strong>Guijama. </strong>
+        Acá encontrarás una amplia selección de los mejores productos
+        relacionados con tabaco, puros, accesorios y más.
+        <br />
+        ¡Explorá nuestros productos y encontrá todo lo que necesitas!
       </p>
 
-      {Object.entries(productsByCategory).map(
-        ([category, categoryProducts]) => (
-          <div className={styles.listContainer} key={category}>
-            <p className={styles.category}>{category}</p>
-            <div className={styles.productList}>
-              {categoryProducts.map((product) => (
-                <div
-                  key={product.code}
-                  className={`${styles.product} ${
-                    product.stock === 0 ? styles.disabled : ""
-                  }`}
-                >
-                  <p>
-                    <strong>{product.title}</strong>
-                  </p>
-                  <p>Marca: {product.brand}</p>
-                  {product.stock === 0 && (
-                    <p className={styles.stockWarning}>Artículo sin stock</p>
-                  )}
-                  <div className={styles.imageContainer}>
-                    {Array.isArray(product.image) &&
-                      product.image.map((imageUrl, index) => (
-                        <img
-                          key={index}
-                          src={imageUrl}
-                          alt={`${product.title}-${index}`}
-                          className={styles.productImage}
-                        />
-                      ))}
+      {sortedCategories.map((category) => (
+        <div className={styles.listContainer} key={category}>
+          <p className={styles.category}>{category}</p>
+          <div className={styles.productList}>
+            {productsByCategory[category].map((product) => (
+              <div
+                key={product.code}
+                className={`${styles.product} ${
+                  product.stock === 0 ? styles.disabled : ""
+                }`}
+              >
+                <p className={styles.title}>
+                  <strong>{product.title}</strong>
+                </p>
+                <p>Marca: {product.brand}</p>
+                {product.stock === 0 && (
+                  <p className={styles.stockWarning}>Artículo sin stock</p>
+                )}
+                <div className={styles.imageContainer}>
+                  {Array.isArray(product.image) &&
+                    product.image.map((imageUrl, index) => (
+                      <img
+                        key={index}
+                        src={imageUrl}
+                        alt={`${product.title}-${index}`}
+                        className={styles.productImage}
+                      />
+                    ))}
+                  <div className={styles.priceContainer}>
+                    <p className={styles.priceLabel}>Precio:</p>
+                    <p className={styles.priceValue}>
+                      <strong>$ {product.priceList}</strong>
+                    </p>
                   </div>
                 </div>
-              ))}
-            </div>
-        
+              </div>
+            ))}
           </div>
-        )
-      )}
+        </div>
+      ))}
     </div>
   );
 };
