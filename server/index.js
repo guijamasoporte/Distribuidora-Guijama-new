d304a72bf54d3946bf8aeaf9f4e0316305da4c55
guijamasoporte <163669239+guijamasoporte@users.noreply.github.com>
2025-09-29 14:20:37 -0300

    Update index.js

--- server/index.js
+++ server/index.js
@@ -17,20 +17,20 @@ const app = express();
 
 const whiteList = [process.env.DEPLOY_CLIENT_URL, "http://localhost:3001","https://distribuidora-guijama-new.vercel.app","https://distribuidora-guijama-new.vercel.app/"];
 
-app.use(
-  cors({
-    origin: function (origin, callback) {
-      console.log("😲😲😲 =>", origin);
-      if (!origin || whiteList.includes(origin)) {
-        return callback(null, origin);
-      }
-      return callback("Error de CORS origin: " + origin + " No autorizado!");
-    },
-    credentials: true,
-  })
-);
+// app.use(
+//   cors({
+//     origin: function (origin, callback) {
+//       console.log("😲😲😲 =>", origin);
+//       if (!origin || whiteList.includes(origin)) {
+//         return callback(null, origin);
+//       }
+//       return callback("Error de CORS origin: " + origin + " No autorizado!");
+//     },
+//     credentials: true,
+//   })
+// );
 
-// app.use(cors());
+app.use(cors());
 app.use(morgan("dev"));
 app.use(express.json());
 app.use(bodyParser.json({ limit: "10mb" }));
